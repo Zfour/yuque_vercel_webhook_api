c36d8cf5193aecbf9b6dc25ba0da6bfbc2465ac1
Zfour <499984532@qq.com>
2021-01-21 00:54:53 +0800

    因为仓库里包含私钥无法获取请求，所以改为url传参

--- api/index.py
+++ api/index.py
@@ -1,16 +1,33 @@
 # -*- coding: UTF-8 -*-
 import requests
+import re
 from http.server import BaseHTTPRequestHandler
+
+def githubCI(token,user,source):
+    requests_path = 'https://api.github.com/repos/' + user + '/' + source + '/dispatches'
+    token = 'token '+token
+    r = requests.post(requests_path,
+    json = {"event_type": "run-it"},
+    headers = {"User-Agent":'curl/7.52.1',
+              'Content-Type': 'application/json',
+              'Accept': 'application/vnd.github.everest-preview+json',
+              'Authorization': token})
+    if r.status_code == 204:
+        return "This's OK!"
+    else:
+        return r.status_code
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        r = requests.post("https://api.github.com/repos/Zfour/my-blog-source-file/dispatches",
-        json = {"event_type": "run-it"},
-        headers = {"User-Agent":'curl/7.52.1',
-              'Content-Type': 'application/json',
-              'Accept': 'application/vnd.github.everest-preview+json',
-              'Authorization': 'token 36d615c7ae1614a74c89ff2bb49842da5b5c382a'})
-        text = str(r)
+        path = self.path
+        token_reg = re.compile(r'token="(.*?)"')
+        user_reg = re.compile(r'user="(.*?)"')
+        source_reg = re.compile(r'source="(.*?)"')
+        token = token_reg.findall(path)
+        user = user_reg.findall(path)
+        source = source_reg.findall(path)
+        text = githubCI(token,user,source)
+        text = str(text)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'text/plain')
