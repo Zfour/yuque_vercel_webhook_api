d44184339b2f4a80655f82770449a475373992e5
Zfour <499984532@qq.com>
2021-01-21 00:24:47 +0800

    111

--- api/index.py
+++ api/index.py
@@ -2,23 +2,15 @@
 import requests
 from http.server import BaseHTTPRequestHandler
 
-def githubCI():
-    r = requests.post("https://api.github.com/repos/Zfour/my-blog-source-file/dispatches",
-    json = {"event_type": "run-it"},
-    headers = {"User-Agent":'curl/7.52.1',
-              'Content-Type': 'application/json',
-              'Accept': 'application/vnd.github.everest-preview+json',
-              'Authorization': 'token d0d73ce3f20f8afc1595eeb4941a9a87b0a83995'})
-    if r.status_code == 204:
-        return "This's OK!"
-    else:
-        return r.status_code
-
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        text = githubCI()
-        text = str(text)
+        r = requests.post("https://api.github.com/repos/Zfour/my-blog-source-file/dispatches",
+        json = {"event_type": "run-it"},
+        headers = {"User-Agent":'curl/7.52.1',
+              'Content-Type': 'application/json',
+              'Accept': 'application/vnd.github.everest-preview+json',
+              'Authorization': 'token 36d615c7ae1614a74c89ff2bb49842da5b5c382a'})
+        text = str(r)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'text/plain')
