a95eb2d138c8112a1626ca1d2fae4c1112d28973
Zfour <499984532@qq.com>
2021-03-01 22:15:52 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,27 +2,53 @@
 import requests
 import re
 from http.server import BaseHTTPRequestHandler
-from bs4 import BeautifulSoup
 
-def github_json(user,repo):
-    requests_path = 'https://github.com/' + user + '/' +repo + '/blob/master/friendlist.json'
-    gitpage = requests.get(requests_path).text
-    soup = BeautifulSoup(gitpage, 'html.parser')
-    main_content = soup.find('td',id = 'LC1').text
-    print(main_content)
+def githubCI(token,user,source):
+    requests_path = 'https://api.github.com/repos/' + user + '/' + source + '/dispatches'
+    token = 'token '+token
+    r = requests.post(requests_path,
+    json = {"event_type": "run-it"},
+    headers = {"User-Agent":'curl/7.52.1',
+              'Content-Type': 'application/json',
+              'Accept': 'application/vnd.github.everest-preview+json',
+              'Authorization': token})
+    if r.status_code == 204:
+        return "This's OK!"
+    else:
+        return r.status_code
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def do_POST(self):
         path = self.path
-        path = path.replace("'", '"')
-        repo_reg = re.compile(r'repo="(.*?)"')
+        path= path.replace("'", '"')
+        token_reg = re.compile(r'token="(.*?)"')
         user_reg = re.compile(r'user="(.*?)"')
+        source_reg = re.compile(r'source="(.*?)"')
+        token = token_reg.findall(path)[0]
         user = user_reg.findall(path)[0]
-        repo = repo_reg.findall(path)[0]
-        data = github_json(user,repo)
+        source = source_reg.findall(path)[0]
+        text = githubCI(token,user,source)
+        text = str(text)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(json.dumps(data).encode('utf-8'))
+        self.wfile.write(text.encode())
+        return
+    def do_GET(self):
+        path = self.path
+        path = path.replace("'", '"')
+        token_reg = re.compile(r'token="(.*?)"')
+        user_reg = re.compile(r'user="(.*?)"')
+        source_reg = re.compile(r'source="(.*?)"')
+        token = token_reg.findall(path)[0]
+        user = user_reg.findall(path)[0]
+        source = source_reg.findall(path)[0]
+        text = githubCI(token,user,source)
+        text = str(text)
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(text.encode())
         return
